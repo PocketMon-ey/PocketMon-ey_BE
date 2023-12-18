d29e0821a264a8a2c7f75ee871fcaff1c82e1ad7
김지훈 <ben224@naver.com>
2023-12-18 11:08:56 +0900

    [추가] 컨트롤러 메서드 추가

--- loan/src/main/java/com/pocketmoney/loan/controller/LoanController.java
+++ loan/src/main/java/com/pocketmoney/loan/controller/LoanController.java
@@ -1,12 +1,14 @@
 package com.pocketmoney.loan.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pocketmoney.loan.dto.LoanListResponseDTO;
+import com.pocketmoney.loan.model.Loan;
 import com.pocketmoney.loan.service.LoanService;
 
 import io.swagger.annotations.ApiOperation;
@@ -14,17 +16,43 @@ import io.swagger.annotations.ApiOperation;
 @RestController
 public class LoanController {
 	
-	@ApiOperation(value = "대출 리스트 조")
-	@GetMapping(value = "/loan/list")
+	@ApiOperation(value = "대출 리스트 조회")
+	@GetMapping(value = "/loan/list/{status}")
 	public ResponseEntity<LoanListResponseDTO> loanList(@PathVariable("status")int status) {
 		LoanService ls = new LoanService();
 		LoanListResponseDTO result = ls.fetchLoanList(status);
 		return new ResponseEntity<LoanListResponseDTO>(result,HttpStatus.OK);
 	}
 	
-	@GetMapping(value = "/loan/listssss")
-	public int loanList() {
-		return 1;
+	@ApiOperation(value = "대출 상세조회")
+	@GetMapping(value = "/loan/{loanid}")
+	public ResponseEntity<Integer> loan(@PathVariable("loanid")int loanId) {
+		return new ResponseEntity<Integer>(loanId,HttpStatus.OK);
 	}
 	
+	@ApiOperation(value = "대출 등록")
+	@PostMapping(value = "/loan")
+	public ResponseEntity<Loan> addLoan(@RequestBody Loan loan) {
+		
+		return new ResponseEntity<Loan>(loan,HttpStatus.OK);
+	}
+	
+	@ApiOperation(value = "대출 심사 승인")
+	@PostMapping(value = "/loan/approve")
+	public ResponseEntity<Integer> approveLoan(@RequestBody int loanId) {
+		return new ResponseEntity<Integer>(loanId,HttpStatus.OK);
+	}
+	
+	
+	@ApiOperation(value = "대출 심사 거절")
+	@PostMapping(value = "/loan/refuse")
+	public ResponseEntity<Integer> refuseLoan(@RequestBody int loanId) {
+		return new ResponseEntity<Integer>(loanId,HttpStatus.OK);
+	}
+	
+	@ApiOperation(value = "대출 납입")
+	@PostMapping(value = "/loan/repayment")
+	public ResponseEntity<Integer> payLoanMoney(@RequestBody int loanId) {
+		return new ResponseEntity<Integer>(loanId,HttpStatus.OK);
+	}
 }
