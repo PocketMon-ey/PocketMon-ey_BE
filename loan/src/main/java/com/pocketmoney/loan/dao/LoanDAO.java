6dfcf675dd88e71a4eeb12e509d113ed0ea23b8d
김지훈 <ben224@naver.com>
2023-12-19 11:05:50 +0900

    [수정] 우리 DB에 맞게 DAO, Mapper 수정

--- loan/src/main/java/com/pocketmoney/loan/dao/LoanDAO.java
+++ loan/src/main/java/com/pocketmoney/loan/dao/LoanDAO.java
@@ -8,7 +8,7 @@ import com.pocketmoney.loan.entity.LoanEntity;
 
 @Mapper
 public interface LoanDAO {
-	List<LoanEntity> selectLoanList(int userId) throws Exception;
+	List<LoanEntity> selectLoanList(int status) throws Exception;
 	LoanEntity selectLoan(int loanId) throws Exception;
 	int insertLoan(LoanEntity loan) throws Exception;
 	int updateLoan(LoanEntity loan) throws Exception;
