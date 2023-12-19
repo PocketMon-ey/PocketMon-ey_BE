0b3e4aedb1f0b5dffff665693b03f547c0b80621
김지훈 <ben224@naver.com>
2023-12-19 11:35:06 +0900

    [추가] LoanEntity 생성자 추가

--- loan/src/main/java/com/pocketmoney/loan/entity/LoanEntity.java
+++ loan/src/main/java/com/pocketmoney/loan/entity/LoanEntity.java
@@ -11,8 +11,28 @@ import lombok.Setter;
 @Setter
 @RequiredArgsConstructor
 public class LoanEntity {
+	public LoanEntity(int id, String reason, int price, int totalPrice, int remainPrice, Double interestRate,
+			int monthlyRepaymentPrice, int period, String startDate, String endDate, int repaymentCnt, int status,
+			String rejectReason, Date createDate, Date updateDate, int childId) {
+		super();
+		this.id = id;
+		this.reason = reason;
+		this.price = price;
+		this.totalPrice = totalPrice;
+		this.remainPrice = remainPrice;
+		this.interestRate = interestRate;
+		this.monthlyRepaymentPrice = monthlyRepaymentPrice;
+		this.period = period;
+		this.startDate = startDate;
+		this.endDate = endDate;
+		this.repaymentCnt = repaymentCnt;
+		this.status = status;
+		this.rejectReason = rejectReason;
+		this.createDate = createDate;
+		this.updateDate = updateDate;
+		this.childId = childId;
+	}
 	int id;
-	int childId;
 	String reason; // 대출 사유 
 	int price; // 총 대출 금액 
 	int totalPrice; // 갚을 금액 
@@ -27,4 +47,7 @@ public class LoanEntity {
 	String rejectReason;
 	Date createDate;
 	Date updateDate;
+	int childId;
+	
+	
 }
