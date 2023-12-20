16aa9f51f23bdf33baa71313e51c55a8447918f0
김지훈 <ben224@naver.com>
2023-12-20 18:10:02 +0900

    [수정] 테이블리스트 계산식 수정

--- loan/src/main/java/com/pocketmoney/loan/dto/TableListRequestDTO.java
+++ loan/src/main/java/com/pocketmoney/loan/dto/TableListRequestDTO.java
@@ -11,10 +11,16 @@ public class TableListRequestDTO {
 	
 	
 	public int getTotalPrice(int month) {
-		return price + price * loanInterest / month;
+		double loanRate = (double)loanInterest / 100;
+		double monthRate = (double)month / 12;
+		System.out.println(loanRate);
+		System.out.println(monthRate);
+		int result = (int)(price + price * loanRate * monthRate) / month * month;
+		return result;
 	}
 	
 	public int getPricePerMonth(int month) {
+		System.out.println(getTotalPrice(month));
 		return getTotalPrice(month) / month;
 	}
 	
