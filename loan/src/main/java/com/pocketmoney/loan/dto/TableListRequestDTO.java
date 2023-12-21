aa3667b442c4f4b0c15da0352a773fc794ff50b1
김지훈 <ben224@naver.com>
2023-12-21 12:19:39 +0900

    [삭제] 대출 테이블 목록 DTO 로직 로그 삭제

--- loan/src/main/java/com/pocketmoney/loan/dto/TableListRequestDTO.java
+++ loan/src/main/java/com/pocketmoney/loan/dto/TableListRequestDTO.java
@@ -13,14 +13,11 @@ public class TableListRequestDTO {
 	public int getTotalPrice(int month) {
 		double loanRate = (double)loanInterest / 100;
 		double monthRate = (double)month / 12;
-		System.out.println(loanRate);
-		System.out.println(monthRate);
 		int result = (int)(price + price * loanRate * monthRate) / month * month;
 		return result;
 	}
 	
 	public int getPricePerMonth(int month) {
-		System.out.println(getTotalPrice(month));
 		return getTotalPrice(month) / month;
 	}
 	
